5a9bd57091ea056bfac5f622d4225585391abae2
Lualmara-5 <luimartinezra@unal.edu.co>
2023-10-09 23:04:01 -0500

    Update Periodico y Tesis

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -1,5 +1,50 @@
 package comunicacion;
 
-public class Periodico {
+public class Periodico extends Escrito{
+    private String fecha;
+    private String primicia;
+    private String interpretacion;
 
+    public Periodico(String origen, String titulo, String autor, int paginas,String fecha, String primicia, String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.fecha = fecha;
+        this.primicia = primicia;
+        this.interpretacion = interpretacion;
+    }
+
+    public String getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getPrimicia() {
+        return this.primicia;
+    }
+
+    public void setPrimicia(String primicia) {
+        this.primicia = primicia;
+    }
+
+    public String getInterpretacion() {
+        return this.interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    public int palabrasTotales(int paginas){
+        return getPaginas()*10*paginas;
+    }
+    
+    public String interpretacion(){
+        return getInterpretacion();
+    }
+    
+    public String toString(){
+        return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+getFecha()+"\n"+getPrimicia();
+    }
 }
--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -1,5 +1,70 @@
 package comunicacion;
 
-public class Tesis {
+public class Tesis extends Escrito{
+    private String idea;
+    private String[] argumentos;
+    private String conclusion;
+    private String referencias;
+    private String interpretacion;
 
+    public Tesis(String origen, String titulo, String autor, int paginas,String idea, String[] argumentos, String conclusion, String referencias, String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.idea=idea;
+        this.argumentos = argumentos;
+        this.conclusion = conclusion;
+        this.referencias = referencias;
+        this.interpretacion = interpretacion;
+    }
+
+    public String getIdea() {
+        return this.idea;
+    }
+
+    public void setIdea(String idea) {
+        this.idea = idea;
+    }
+
+    public String[] getArgumentos() {
+        return this.argumentos;
+    }
+
+    public void setArgumentos(String[] argumentos) {
+        this.argumentos = argumentos;
+    }
+
+    public String getConclusion() {
+        return this.conclusion;
+    }
+
+    public void setConclusion(String conclusion) {
+        this.conclusion = conclusion;
+    }
+
+    public String getReferencias() {
+        return this.referencias;
+    }
+
+    public void setReferencias(String referencias) {
+        this.referencias = referencias;
+    }
+
+    public String getInterpretacion() {
+        return this.interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    public int palabrasTotales(int paginas){
+        return getPaginas()*5*paginas;
+    }
+    
+    public String interpretacion(){
+        return getInterpretacion();
+    }
+    
+    public String toString(){
+        return getOrigen()+"\n"+getTitulo()+"\n"+getAutor()+"\n"+getPaginas()+"\n"+getIdea()+"\n"+argumentos.length+"\n"+getConclusion()+"\n"+getReferencias();
+    }
 }
